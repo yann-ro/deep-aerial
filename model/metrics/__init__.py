a440fa78359ceb4a75074be3a25e61ff84f0abf7
YannRodriguez <52059549+YannRodriguez@users.noreply.github.com>
2023-02-20 17:08:17 +0100

    fix import

--- model/metrics/__init__.py
+++ model/metrics/__init__.py
@@ -1,3 +1,4 @@
 from .plot_metrics import plot_acc, plot_loss, plot_score
-from .miou import mIoU
-from .pixel_accuracy import pixel_accuracy
+from .segmentation import mIoU, pixel_accuracy
+
+__all__ = ["plot_acc", "plot_loss", "plot_score", "mIoU", "pixel_accuracy"]
